2b4c79ff8c1a6f69f5c99e56cbd7b78391353fdf
sadek1999 <sadekali1530@gmail.com>
2024-11-25 22:37:48 +0600

    make AuthuserResores and userResouser

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,11 +41,8 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
